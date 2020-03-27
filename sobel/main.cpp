054ea6475af0d8ca0b1fda46bac0e0945801c58c
BRIGHTUR <50517987+BRIGHTUR@users.noreply.github.com>
2020-03-27 15:00:19 +0800

    加了converabs

--- sobel/main.cpp
+++ sobel/main.cpp
@@ -16,10 +16,14 @@ int main()
 		Mat fframe;
 		Mat dx;
 		Mat dy;
+		Mat abs_dx;
+		Mat abs_dy;
 		Sobel(frame, dx, CV_16SC1, 1, 0, 3);
 		Sobel(frame, dy, CV_16SC1, 0, 1, 3);
-		imshow("x", dx);
-		imshow("y", dy);
+		convertScaleAbs(dx, abs_dx);
+		convertScaleAbs(dy, abs_dy);
+		imshow("x", abs_dx);
+		imshow("y", abs_dy);
 		waitKey(30);
 	}
 }
